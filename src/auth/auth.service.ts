d7ff0a7def99ea2fe334599253be97debe6a66c9
Christian Roberto <christian.roberto.g@gmail.com>
2024-05-18 23:29:46 -0600

    Login - comparar y verificar correo y contraseña con bd

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,11 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from "@nestjs/common";
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from "@nestjs/common";
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { InjectModel } from "@nestjs/mongoose";
 import { User } from "./entities/user.entity";
 import { Model } from "mongoose";
 import * as bcryptjs from "bcryptjs";
+import { LoginDto } from "./dto/login.dto";
 
 @Injectable()
 export class AuthService {
@@ -37,6 +38,33 @@ export class AuthService {
       throw new InternalServerErrorException('Something terrible happen')
     }
 
+  }
+
+  async login(loginDto: LoginDto){
+    const {email, password} = loginDto;
+
+    const user = await this.userModel.findOne({email});
+    if(!user){
+      throw new UnauthorizedException('Not valid credentials - Email');
+    }
+
+    if(!bcryptjs.compareSync(password, user.password)){
+      throw new UnauthorizedException('Not valid credentials - Password');
+    }
+
+    const {password:_, ...rest} = user.toJSON();
+
+
+
+    return {
+      user: rest,
+      token: 'ABC-123'
+    }
+    /**
+     * User{id, name, roles}
+     *Token ->
+     *
+     */
   }
 
   findAll() {
