9a8f60b1f442ef269bf3992b0dbe9935350bb18f
Christian Roberto <christian.roberto.g@gmail.com>
2024-05-25 23:05:02 -0600

    Validar JWT y obtener usuario

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -1,22 +1,43 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
+import { JwtPayload } from "../Interfaces/jwt-payload";
+import * as process from "process";
+import { AuthService } from "../auth.service";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
   constructor(
-    private jwtService: JwtService
+    private jwtService: JwtService,
+    private  authService: AuthService,
   ) {
   }
-  canActivate(
+  async canActivate(
     context: ExecutionContext,
   ):Promise<boolean>  {
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException('There is not bearer token ');
+    }
+    try {
+      const payload = await this.jwtService.verifyAsync<JwtPayload>(
+        token,
+        {
+          secret: process.env.JWT_SEED
+        }
+      );
+      const user = await  this.authService.findUserById(payload.id)
+      if(!user) throw new UnauthorizedException('User does not exist')
+      if(!user.isActive) throw new UnauthorizedException('User is not active')
 
-    console.log({token});
-    return Promise.resolve(true);
+      request['user'] = user;
+    } catch {
+      throw new UnauthorizedException();
+    }
+
+
+    return true;
   }
   private extractTokenFromHeader(request: Request): string | undefined {
     const [type, token] = request.headers['authorization']?.split(' ') ?? [];
